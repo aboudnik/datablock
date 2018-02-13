88a9169fab6a3b68c6fd83a8f118170f5785ef6f
Alexandre Boudnik <Alexandre_Boudnik@epam.com>
2018-02-13 02:42:33 -0500

    structure

---
+++ core/src/main/java/org/boudnik/datablock/core/Main.java
@@ -0,0 +1,11 @@
+package org.boudnik.datablock.core;
+
+/**
+ * @author Alexandre_Boudnik
+ * @since 02/13/2018
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+    }
+}
---
+++ core/src/test/java/org/boudnik/datablock/core/MainTest.java
@@ -0,0 +1,14 @@
+package org.boudnik.datablock.core;
+
+import org.junit.Test;
+
+/**
+ * @author Alexandre_Boudnik
+ * @since 02/13/2018
+ */
+public class MainTest {
+
+    @Test
+    public void main() {
+    }
+}
