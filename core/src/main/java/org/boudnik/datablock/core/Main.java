6a5b091d90ca1e4eb781d233a088ee1e6924a035
Alexandre Boudnik <Alexandre_Boudnik@epam.com>
2018-03-01 13:40:59 -0500

    new root

--- core/src/main/java/org/boudnik/datablock/core/Main.java
+++ core/src/main/java/org/boudnik/datablock/core/Main.java
@@ -1,11 +1,14 @@
 package org.boudnik.datablock.core;
 
+import org.boudnik.framework.Store;
+
 /**
  * @author Alexandre_Boudnik
  * @since 02/13/2018
  */
 public class Main {
     public static void main(String[] args) {
+        Store instance = Store.instance();
         System.out.println("Main.main");
     }
 }
